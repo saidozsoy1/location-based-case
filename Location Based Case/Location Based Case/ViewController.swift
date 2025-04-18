1b0034b01a65a2197b5c96e43bdf5a52222ddfdf
Said Ozsoy <said@poilabs.com>
2025-04-18 14:56:54 +0300

    Create Project

---
+++ Location Based Case/Location Based Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location Based Case
+//
+//  Created by Said Ozsoy on 18.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
